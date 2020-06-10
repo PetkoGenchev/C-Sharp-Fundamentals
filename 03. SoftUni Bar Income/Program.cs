e351fb331ebc73b638835be15ce6581c553c1d91
PetkoGenchev <petko.genchev@yahoo.com>
2020-06-10 16:33:53 +0300

    First Commit

---
+++ 03. SoftUni Bar Income/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Runtime.ExceptionServices;
+using System.Diagnostics.Tracing;
+
+namespace PetkoCSharp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string code = @"%([A-Z][a-z]+)%[^|%$.]*<(\w+)>[^|%$.]*\|(\d+)\|[^|%$.]*?(\d+\.?\d*)\$";
+            Regex regex = new Regex(code);
+
+            double income = 0;
+
+            while (true)
+            {
+                string entry = Console.ReadLine();
+
+                if (entry == "end of shift")
+                {
+                    Console.WriteLine($"Total income: {income:F2}");
+                    break;
+                }
+
+                Match match = regex.Match(entry);
+
+                if (match.Success)
+                {
+                    double add = int.Parse(match.Groups[3].Value) * double.Parse(match.Groups[4].Value);
+                    income += add;
+                    Console.WriteLine($"{match.Groups[1].Value}: {match.Groups[2].Value} - {add:F2}");
+
+                }
+
+            }
+        }
+    }
+}
---
+++ 04. Star Enigma/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Runtime.ExceptionServices;
+using System.Diagnostics.Tracing;
+
+namespace PetkoCSharp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string code = @"\S*@([A-Z][a-z]+)[^@\-!:>]*:([\d]+)!([AD])![^@\-!:>]*->([\d]+)";
+            Regex regex = new Regex(code);
+
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<string>> planets = new Dictionary<string, List<string>>
+            {
+                { "Attacked", new List<string>() },
+                { "Destroyed", new List<string>() }
+            };
+
+            for (int i = 0; i < n; i++)
+            {
+                string entry = Console.ReadLine();
+                int count = Regex.Matches(entry, @"S|T|A|R|s|t|a|r").Count;
+
+                StringBuilder finalText = new StringBuilder();
+
+                char[] entryChar = entry
+                    .ToCharArray();
+
+                for (int j = 0; j < entryChar.Length; j++)
+                {
+                    char lettertoUse = Convert.ToChar(entryChar[j] - count);
+
+                    finalText.Append(lettertoUse);
+                }
+
+                string notBuilder = finalText.ToString();
+
+                Match match = regex.Match(notBuilder);
+
+                if (match.Success)
+                {
+                    if (match.Groups[3].Value == "A")
+                    {
+                        planets["Attacked"].Add(match.Groups[1].Value);
+                    }
+                    else
+                    {
+                        planets["Destroyed"].Add(match.Groups[1].Value);
+                    }
+                }
+
+            }
+
+            foreach (var item in planets)
+            {
+                Console.WriteLine($"{item.Key} planets: {item.Value.Count}");
+                foreach (var what in item.Value.OrderBy(x => x))
+                {
+                    Console.WriteLine($"-> {what}");
+                }
+            }
+
+
+        }
+    }
+}
